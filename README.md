e3262d492302b0155e09e432fae740897bc6632c
Szymon Lipiński <mabewlun@gmail.com>
2020-08-20 12:18:18 +0200

    Add benches reports

--- README.md
+++ README.md
@@ -4,6 +4,24 @@ An implementation of BitSet in Rust.
 All is fully tested, including random tests with quickcheck.
 
 
+# Benchmarks
+
+I made simple benchmarks using `cargo bench`.
+
+They check the speed of some simple bit operations on bitsets implemented as:
+
+- a single u64 variable
+- an array with a single u64 variable
+- a list with a single u64 variable
+
+There are two versions of each function checked: inlined and not inlined.
 
 
+## Benchmark Results
 
+The results are at [report/Bits/report/index.html](report/Bits/report/index.html).
+
+The interesting parts are:
+
+- the speed of the not inlined vector is 4 times smaller than the speed of the rest of the functions
+- the speed of the inlined vector implementation is the same as the speed of the single variable one
--- benches/array_speed.rs
+++ benches/array_speed.rs
@@ -1,7 +1,7 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
 #[inline]
-fn simple_bits() -> u64 {
+fn simple_bits_inline() -> u64 {
     let mut value: u64 = 0;
 
     // set all bits one by one
@@ -15,8 +15,21 @@ fn simple_bits() -> u64 {
     value
 }
 
+fn simple_bits() -> u64 {
+    let mut value: u64 = 0;
+
+    // set all bits one by one
+    for i in 0..63 {
+        value = value | (1 << i);
+    }
+    // reset all bits one by one
+    for i in 0..63 {
+        value = value & !(1 << i);
+    }
+    value
+}
 #[inline]
-fn array_bits() -> u64 {
+fn array_bits_inline() -> u64 {
     let mut value: [u64; 1] = [0];
     // set all bits one by one
     for i in 0..63 {
@@ -29,7 +42,32 @@ fn array_bits() -> u64 {
     value[0]
 }
 
+fn array_bits() -> u64 {
+    let mut value: [u64; 1] = [0];
+    // set all bits one by one
+    for i in 0..63 {
+        value[0] = value[0] | (1 << i);
+    }
+    // reset all bits one by one
+    for i in 0..63 {
+        value[0] = value[0] & !(1 << i);
+    }
+    value[0]
+}
 #[inline]
+fn vector_bits_inline() -> u64 {
+    let mut value: Vec<u64> = vec![0];
+    // set all bits one by one
+    for i in 0..63 {
+        value[0] = value[0] | (1 << i);
+    }
+    // reset all bits one by one
+    for i in 0..63 {
+        value[0] = value[0] & !(1 << i);
+    }
+    value[0]
+}
+
 fn vector_bits() -> u64 {
     let mut value: Vec<u64> = vec![0];
     // set all bits one by one
@@ -48,6 +86,9 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("simple_bits", |b| b.iter(|| simple_bits()));
     c.bench_function("array_bits", |b| b.iter(|| array_bits()));
     c.bench_function("vector_bits", |b| b.iter(|| vector_bits()));
+    c.bench_function("simple_bits_inline", |b| b.iter(|| simple_bits_inline()));
+    c.bench_function("array_bits_inline", |b| b.iter(|| array_bits_inline()));
+    c.bench_function("vector_bits_inline", |b| b.iter(|| vector_bits_inline()));
     c.finish();
 }
 
